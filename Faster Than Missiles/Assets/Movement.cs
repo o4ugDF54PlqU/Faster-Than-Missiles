61640d100a07b3250000cbc824fe6c7c8886008f
Plorthos <tlkb@protonmail.com>
2023-07-16 16:55:35 -0400

    polished movement

--- Faster Than Missiles/Assets/Movement.cs
+++ Faster Than Missiles/Assets/Movement.cs
@@ -8,6 +8,11 @@ public class Movement : MonoBehaviour
     public bool thrusting = false;
     public SpriteRenderer exhaustSprite;
     public float rotationSpeed = 180f;
+    public float drag = 1;
+    public float angularDrag = 100;
+    public float thrustForce = 1000;
+    public float maxVelocity = 10;
+    public float maxAngularVelocity = 10;
     private bool turningLeft = false;
     private bool turningRight = false;
     private Rigidbody2D playerRigidbody;
@@ -16,13 +21,15 @@ public class Movement : MonoBehaviour
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerRigidbody.drag = drag;
+        playerRigidbody.angularDrag = angularDrag;
     }
 
     void Update()
     {
         if (thrusting)
         {
-            playerRigidbody.AddForce(transform.up* 500f * Time.deltaTime);
+            playerRigidbody.AddForce(transform.up * thrustForce * Time.deltaTime);
         }
         else
         {
@@ -39,15 +46,32 @@ public class Movement : MonoBehaviour
         }
     }
 
+    void FixedUpdate()
+    {
+        if (playerRigidbody.velocity.magnitude > maxVelocity)
+        {
+            playerRigidbody.velocity = playerRigidbody.velocity.normalized * maxVelocity;
+        }
+        if (Mathf.Abs(playerRigidbody.angularVelocity) > maxAngularVelocity)
+        {
+            if (playerRigidbody.angularVelocity > 0)
+            playerRigidbody.angularVelocity = maxAngularVelocity;
+            else
+            playerRigidbody.angularVelocity = -maxAngularVelocity;
+        }
+    }
+
     public void Thrust(InputAction.CallbackContext context)
     {
         if (context.action.IsPressed())
         {
+            playerRigidbody.drag = 0.3f;
             thrusting = true;
             exhaustSprite.enabled = true;
         }
         else
         {
+            playerRigidbody.drag = drag;
             thrusting = false;
             exhaustSprite.enabled = false;
         }
@@ -57,10 +81,12 @@ public class Movement : MonoBehaviour
     {
         if (context.action.IsPressed())
         {
+            playerRigidbody.angularDrag = 0.01f;
             turningLeft = true;
         }
         else
         {
+            playerRigidbody.angularDrag = angularDrag;
             turningLeft = false;
         }
     }
@@ -69,10 +95,12 @@ public class Movement : MonoBehaviour
     {
         if (context.action.IsPressed())
         {
+            playerRigidbody.angularDrag = 0.01f;
             turningRight = true;
         }
         else
         {
+            playerRigidbody.angularDrag = angularDrag;
             turningRight = false;
         }
     }
